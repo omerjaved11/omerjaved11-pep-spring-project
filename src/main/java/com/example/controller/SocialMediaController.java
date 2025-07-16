7d0e2143f28dbbd39ced9ae4794c2e9d79f74052
omer javed <omerjaved11@gmail.com>
2025-07-16 20:58:21 +0000

    Register module completed

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,19 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.entity.Message;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +21,69 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+
+    @PostMapping("register")
+    ResponseEntity<Account> registerUser(@RequestBody Account reqAccount){
+        Account respAccount =  null;
+
+        return ResponseEntity.ok().body(respAccount);
+    }
+
+    
+    @PostMapping("login")
+    ResponseEntity<Account> loginUser(@RequestBody Account reqAccount){
+        Account respAccount =  null;
+
+        return ResponseEntity.ok().body(respAccount);
+    }
+
+    
+    @PostMapping("messages")
+    ResponseEntity<Message> registerUser(@RequestBody Message message){
+        Message respMessage =  null;
+
+        return ResponseEntity.ok().body(respMessage);
+    }
+
+     
+    @GetMapping("messages")
+    ResponseEntity<List<Message>> getAllMessages(){
+        List<Message> allMessages = null;
+
+        return ResponseEntity.ok().body(allMessages);
+    }
+
+      
+    @GetMapping("messages/{messageId}")
+    ResponseEntity<Message> getMessage(@PathVariable long messageId){
+        Message message = null;
+
+        return ResponseEntity.ok().body(message);
+    }
+
+      
+    @DeleteMapping("messages/{messageId}")
+    ResponseEntity<Message> deleteMessage(@PathVariable long messageId){
+        Message message = null;
+
+        return ResponseEntity.ok().body(message);
+    }
+    
+          
+    @PatchMapping("messages/{messageId}")
+    ResponseEntity<Message> patchMessage(@PathVariable long messageId){
+        Message message = null;
+
+        return ResponseEntity.ok().body(message);
+    }
+
+    @GetMapping("accounts/{accountId}/messages")
+    ResponseEntity<Message> getMessagesByAccountId(@PathVariable long accountId){
+        Message message = null;
+
+        return ResponseEntity.ok().body(message);
+    }
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,36 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+import javassist.bytecode.DuplicateMemberException;
+
+@Service
 public class AccountService {
+
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account registerUser(Account account){
+        
+
+
+        if(null == account.getUsername() || account.getUsername().isEmpty() || account.getUsername().isBlank())
+            throw new IllegalArgumentException(account.getUsername() + " Username cannot be empty or blank");
+        
+        if(null == account.getPassword() || account.getPassword().length() < 4)
+            throw new IllegalArgumentException(account.getPassword() + " Password cannot be less than 4 character");
+            
+        if(accountRepository.existsByUsername(account.getUsername()))
+            throw new IllegalArgumentException(account.getUsername() + " already exists");
+
+        return accountRepository.save(account);
+    }
 }
